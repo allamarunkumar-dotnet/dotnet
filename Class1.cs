2a90b7b5bc2b2262533fd8ee996b89ef0919b93d
allamarunkumar-dotnet <78468146+allamarunkumar-dotnet@users.noreply.github.com>
2021-02-03 12:51:09 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OjasDotNetEmployee2Batch
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 10;
+            int num2 = 20;
+            int sum = num1 + num2;
+            int sub = num1 - num2;
+            int mul = num1 * num2;
+            Console.WriteLine("{0}  {1}   {2}", sum, sub, mul);
+            Console.WriteLine($"{sum}  {sub}   {mul}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks; // Predefined Namespaces or .dll files
+
+namespace OjasDotNetEmployee2Batch // User defined Namespace
+{
+    class Program // class declararion
+    {
+        static void Main(string[] args) // method
+        {
+            int num1 = 10;
+            int num2 = 20;
+            int sum = num1 + num2;
+            int sub = num1 - num2;
+            int mul = num1 * num2;
+            System.Console.WriteLine(sum);
+            System.Console.WriteLine(sub);
+            System.Console.WriteLine(mul);
+            System.Console.WriteLine(num1/num2);
+        }
+    }
+}
