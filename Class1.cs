441ab6cd4a9992b54feeaeb19076a34df7ca1142
allamarunkumar-dotnet <78468146+allamarunkumar-dotnet@users.noreply.github.com>
2021-02-08 17:43:30 +0530

    Add files via upload

--- Class1.cs
+++ Class1.cs
@@ -4,19 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ArunConsoleApp
+namespace KiranConsoleApp
 {
     class Class1
     {
         static void Main(string[] args)
         {
-            int num = 1238;
-            while (num != 0)//123 != 0   12 != 0  1!=0 0 != 0
-            {
-                int digit = num % 10;//digit = 3 digit = 2 digit = 1
-                Console.WriteLine(digit);//3  2 1
-                num = num / 10;// num = 12  num = 1 num = 0
-            }
+            string s = "ojas";
+            Console.WriteLine(s);//lower ojas
+            string k = s.ToUpper();//
+            Console.WriteLine(k);
+            Console.WriteLine(s);//Upper  
+
         }
     }
 }
--- Class2.cs
+++ Class2.cs
@@ -4,21 +4,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ArunConsoleApp
+namespace KiranConsoleApp
 {
     class Class2
     {
         static void Main(string[] args)
         {
-            int num = 546;
-            int sum = 0;
-            while (num > 0)
-            {
-                int digit = num % 10;
-                sum = sum + digit;
-                num = num / 10;
-            }
-            Console.WriteLine("Sum of the digits :{0}",sum);
+            string str = "welcome";
+            Console.WriteLine(str.ElementAt(0));
+            Console.WriteLine(str.IndexOf('l'));
+            Console.WriteLine(str.Contains("me"));
+            Console.WriteLine(str.Substring(0));
+            Console.WriteLine(str.Substring(1));
+            Console.WriteLine(str.Substring(2));
+            Console.WriteLine(str.Length);
+            Console.WriteLine(str.Substring(0,str.Length));
+            Console.WriteLine(str.Substring(0, str.Length-1));
+            Console.WriteLine(str.Substring(0,str.Length-2));
+            string dob = "9-June-1996";
+            string[] res =dob.Split('-');
+            Console.WriteLine(res[0]);
+            Console.WriteLine(res[1]);
+            Console.WriteLine(res[2]);
+
+            Console.WriteLine("aaaa".CompareTo("aaaa"));
+            Console.WriteLine(str.Equals("elcome"));
+
+
+
         }
     }
 }
+
+
+
+
+
+
+
+
+
